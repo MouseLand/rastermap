a20367aa58cf1e161fd122f08e5261daf127ece0
Carsen Stringer <carsen.stringer@gmail.com>
2018-08-19 17:58:10 -0400

    y

--- map/map.py
+++ map/map.py
@@ -57,11 +57,14 @@ def map(S, ops=None, u=None, sv=None):
     vsmooth = V.T @ v[iPC,:]
     return isort, vsmooth
 
-def run_map(S,ops=None):
-    isort2 = map(S.T,ops)
+def main(S,ops=None,u=None,sv=None):
+    isort2,V = map(S.T,ops,u,sv)
     Sm = S - S.mean(axis=1)
     Sm = gaussian_filter1d(Sm,5,axis=1)
-    isort1 = map(Sm,ops)
+    isort1,V = map(Sm,ops,u,sv)
     ns = 0.02 * Sm.shape[0]
     Sm = gaussian_filter1d(Sm[isort1,:],ns,axis=1)
-    return isort1,isort2,Sm,V
+    return Sm,isort1,isort2
+
+if __name__ == "__main__":
+    main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
