837372e68e7a89d1eac24991857490add7957b43
carsen-stringer <carsen.stringer@gmail.com>
2018-08-21 09:45:06 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rastermap",
+    version="0.0.1",
+    author="Marius Pachitariu and Carsen Stringer",
+    author_email="carsen.stringer@gmail.com",
+    description="Unsupervised clustering algorithm for 2D data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MouseLand/suite2p",
+    packages=setuptools.find_packages(),
+	install_requires = ['numpy', 'scipy'],
+    include_package_data=True,
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ),
+)
