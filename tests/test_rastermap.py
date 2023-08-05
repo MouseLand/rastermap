9fc08c92daf9698d09c67ea5fa3524a153121ca4
Carsen Stringer <carsen.stringer@gmail.com>
2023-08-05 09:54:08 -0400

    adding test for n_splits > 0

--- tests/test_rastermap.py
+++ tests/test_rastermap.py
@@ -12,3 +12,14 @@ def test_rastermap(test_file):
     assert hasattr(model, "isort")
     assert hasattr(model, "Usv")
     assert hasattr(model, "Vsv")
+
+def test_rastermap_splits(test_file):
+    dat = np.load(test_file)
+    spks = dat["spks"]
+
+    model = Rastermap(n_splits=2, n_clusters=20, nc_splits=10).fit(data=spks)
+
+    assert hasattr(model, "embedding")
+    assert hasattr(model, "isort")
+    assert hasattr(model, "Usv")
+    assert hasattr(model, "Vsv")
